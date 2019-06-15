807a6306f7bd22be5ec11ed327f4791d4dbadb83
Daniel Cotter <daniel.cotter@uky.edu>
2019-06-15 17:10:42 -0400

    Changed FHIR input/encoder from lab results to encounter and implemented message generation & serialization to XML and deserialization back to FHIR resource

--- src/main/java/Launcher.java
+++ src/main/java/Launcher.java
@@ -1,6 +1,8 @@
+import encoder.FHIREncounterEncoder;
 import encoder.HL7VitalSignsEncoder;
 import encoder.IEncoder;
 import encoder.PipeDelimitedPatientsEncoder;
+import input.FHIREncounterInput;
 import input.HL7VitalSignsInput;
 import input.IInput;
 import input.PipeDelimitedPatientsInput;
@@ -16,6 +18,7 @@ public class Launcher {
         /* INPUTS */
         IInput input1 = new HL7VitalSignsInput("INPUT.VITALS.HL7");
         IInput input2 = new PipeDelimitedPatientsInput("INPUT.PATIENTS.CSV");
+        IInput input3 = new FHIREncounterInput("INPUT.ENCOUNTERS.FHIR");
 
         // Add FHIR producer p2
         // * Use the HAPITester class in the examples package to download FHIR
@@ -24,6 +27,7 @@ public class Launcher {
         /* ENCODERS */
         IEncoder e1 = new HL7VitalSignsEncoder("INPUT.VITALS.HL7","STORE.TDB");
         IEncoder e2 = new PipeDelimitedPatientsEncoder("INPUT.PATIENTS.CSV","STORE.TDB");
+        IEncoder e3 = new FHIREncounterEncoder("INPUT.ENCOUNTERS.FHIR","STORE.TDB");
 
         /* STORE */
         IStore s1 = new TDBStore("STORE.TDB","QUERY");
@@ -40,6 +44,7 @@ public class Launcher {
         // * Add OMOP output
 
         // Start sending messages after the pipeline is set up
-        input1.start();
+//        input1.start();
+        input3.start();
     }
 }
