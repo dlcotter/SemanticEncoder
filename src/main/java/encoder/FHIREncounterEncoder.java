255f1d015c3ae00262160ed68b93557dd59c1e58
Daniel Cotter <daniel.cotter@uky.edu>
2019-06-15 17:10:22 -0400

    Changed FHIR input/encoder from lab results to encounter and implemented message generation & serialization to XML and deserialization back to FHIR resource

---
+++ src/main/java/encoder/FHIREncounterEncoder.java
@@ -0,0 +1,33 @@
+package encoder;
+
+import ca.uhn.fhir.context.FhirContext;
+import org.apache.jena.rdf.model.Model;
+import org.hl7.fhir.dstu3.model.Encounter;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FHIREncounterEncoder extends Encoder implements IEncoder {
+    public FHIREncounterEncoder(String inputTopicName, String outputTopicName) {
+        super(inputTopicName, outputTopicName);
+    }
+
+    @Override
+    public List<Model> buildModel(String message) {
+        FhirContext ctx = FhirContext.forDstu3();
+        IBaseResource resource = ctx.newXmlParser().parseResource(message);
+
+        if (!(resource instanceof Encounter))
+            return new ArrayList<>();
+
+        ArrayList<Model> models = new ArrayList<>();
+
+        Encounter FHIREncounter = (Encounter) resource;
+        common.Encounter commonEncounter = new common.Encounter();
+        commonEncounter.identifier = FHIREncounter.getIdentifier().get(0).getValue();
+        models.add(encodeEncounter(commonEncounter));
+
+        return models;
+    }
+}
---
+++ src/main/java/input/FHIREncounterInput.java
@@ -0,0 +1,50 @@
+package input;
+
+import ca.uhn.fhir.context.FhirContext;
+import org.hl7.fhir.dstu3.model.Encounter;
+
+import java.util.Date;
+
+public class FHIREncounterInput extends Input {
+    public FHIREncounterInput(String outputTopicName) {
+        super(outputTopicName);
+    }
+
+    @Override
+    public String getNextMessage() {
+        Encounter encounter = new Encounter();
+        encounter.setId("1961851");
+        encounter.addIdentifier()
+                    .setSystem("FDS-EncounterId")
+                    .setValue("25");
+        encounter.setStatus(Encounter.EncounterStatus.FINISHED);
+        encounter.addType()
+                    .setText("Encounter for problem (procedure)")
+                    .addCoding()
+                        .setSystem("http://snomed.info/sct")
+                        .setCode("185347001")
+                        .setDisplay("Encounter for problem (procedure)");
+        encounter.getSubject()
+                    .setReference("Patient/1961849")
+                    .setDisplay("Sample Test");
+        encounter.addParticipant()
+                    .getIndividual()
+                        .setReference("Practitioner/1961850")
+                        .setDisplay("Amanda Applegate");
+        encounter.getPeriod()
+                    .setStart(new Date("06/14/2019"))
+                    .setEnd(new Date("06/14/2019"));
+        encounter.addReason()
+                .setText("Medication education (procedure)")
+                .addCoding()
+                    .setSystem("http://snomed.info/sct")
+                    .setCode("967006")
+                    .setDisplay("Medication education (procedure)");
+
+        // Serialize the message
+        FhirContext ctx = FhirContext.forDstu3();
+        String output = ctx.newXmlParser().setPrettyPrint(true).encodeResourceToString(encounter);
+
+        return output;
+    }
+}
