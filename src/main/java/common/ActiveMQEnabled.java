cd84b3fd534bc5cc42352b7a744582dfd1a86e75
Daniel Cotter <daniel.cotter@uky.edu>
2019-06-28 11:06:35 -0400

    Added classes VitalSign and VitalSignSet to support simulating different conditions (hypotension, hypothermia, etc.)

--- src/main/java/common/ActiveMQEnabled.java
+++ src/main/java/common/ActiveMQEnabled.java
@@ -5,6 +5,9 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import javax.jms.*;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public abstract class ActiveMQEnabled {
     private ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
@@ -14,7 +17,10 @@ public abstract class ActiveMQEnabled {
     protected MessageProducer producer;
     protected String inputTopicName, outputTopicName;
     private Destination inputDestination, outputDestination;
-    private boolean debug = true;
+    private static final String logDirectory = "/home/dcotter/mscs/610-masters-project/logs/";
+    boolean logDebugInfo = true, printDebugInfo = false, includeMessageContents = false;
+    private FileHandler fh;
+    private Logger logger;
 
     public ActiveMQEnabled(String inputTopicName, String outputTopicName){
         if (inputTopicName == null && outputTopicName == null)
@@ -43,35 +49,45 @@ public abstract class ActiveMQEnabled {
         catch (Exception e) {
             e.printStackTrace();
         }
-    }
 
-    public void setDebug(boolean value) {
-        this.debug = value;
-    }
-
-    protected void printMessageDebugInfo(TextMessage textMessage, String receiptMode) {
-        if (!debug)
-            return;
-
-        // Compose debug message from various pieces of information
-        // Note: Better to compose the string first and then call println() than to call
-        // println() repeatedly, since each call to println() is executed separately and
-        // can wind up interleaved with other threads' printed statements in a jumble.
-        String debugMessage = "\n"
-            + this.getClass() + " " + receiptMode + " message.\n"
-            + "Time: " + new Date() + "\n"
-            + "Input topic: " + inputTopicName + "\n"
-            + "Output topic: " + outputTopicName + "\n"
-            + "Message hash: " + textMessage.hashCode() + "\n"
-            + "Current thread: " + Thread.currentThread().getName() + "\n";
-
+        // Configure the file handler and text formatter
+        String thisClassName = this.getClass().getName();
         try {
-            debugMessage += "Message contents: " + textMessage.getText();
-        } catch (JMSException e) {
+            fh = new FileHandler(logDirectory + thisClassName + ".txt", true /* append */);
+            fh.setFormatter(new SimpleFormatter());
+            logger = Logger.getLogger(thisClassName);
+            logger.addHandler(fh);
+            logger.info("TIME|CLASS|EVENT|INPUT_TOPIC|OUTPUT_TOPIC|MSG_HASH|THREAD");
+        } catch (Exception e) {
             e.printStackTrace();
+            return;
         }
+    }
 
-        System.out.println(debugMessage);
+    protected void finalize() {
+        fh.close();
+    }
+
+    protected void logMessage(TextMessage textMessage, String receiptMode) {
+        // Format the log entry
+        String[] fields = new String[] {
+                 String.valueOf(new Date())             // TIME
+                ,this.getClass().getName()              // CLASS
+                ,receiptMode                            // EVENT
+                ,inputTopicName                         // INPUT_TOPIC
+                ,outputTopicName                        // OUTPUT_TOPIC
+                ,String.valueOf(textMessage.hashCode()) // MSG_HASH
+                ,Thread.currentThread().getName()       // THREAD
+                //textMessage.getText()                 // message contents - not sure how to enable multiline message content debugging
+        };
+        String logInfo = String.join("|", fields);
+
+        // Log to the subscribed destinations
+        if (logDebugInfo)
+                logger.info(logInfo);
+
+        if (printDebugInfo)
+            System.out.println(logInfo);
     }
 
     // To be implemented by child classes for use in incomingMessageHandler (below)
@@ -80,6 +96,7 @@ public abstract class ActiveMQEnabled {
     private MessageListener incomingMessageHandler = new MessageListener() {
         @Override
         public void onMessage(Message inputMessage) {
+            // Die if wrong type of message
             if (!(inputMessage instanceof TextMessage))
                 return;
 
@@ -87,11 +104,12 @@ public abstract class ActiveMQEnabled {
             String inputMessageText = "";
             try {
                 inputMessageText = ((TextMessage) inputMessage).getText();
-                printMessageDebugInfo((TextMessage)inputMessage, "caught");
+                logMessage((TextMessage)inputMessage, "caught");
             } catch(JMSException e) {
                 e.printStackTrace();
             }
 
+            // Die if empty message
             if (inputMessageText.isEmpty())
                 return;
 
@@ -103,12 +121,12 @@ public abstract class ActiveMQEnabled {
                 if (outputMessageText.isEmpty())
                     continue;
 
-                // Build message
+                // Build output message
                 TextMessage outputMessage;
                 try {
                     outputMessage = session.createTextMessage(outputMessageText);
                     producer.send(outputMessage);
-                    printMessageDebugInfo(outputMessage, "sent");
+                    logMessage(outputMessage, "sent");
                 } catch (JMSException e) {
                     e.printStackTrace();
                 }
