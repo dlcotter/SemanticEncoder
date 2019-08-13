8edba458359e16d75a9b3b82d7356371b724105e
dlcotter <cotter.daniel@gmail.com>
2019-08-13 09:13:32 -0400

    Got rid of Store superclass (wasn't adding anything to TDBStore and wasn't needed since there was only one subclass).

--- src/main/java/store/Store.java
+++
@@ -1,9 +0,0 @@
-package store;
-
-import common.ActiveMQEnabled;
-
-public abstract class Store extends ActiveMQEnabled {
-    Store(String inputTopicName, String outputTopicName) {
-        super(inputTopicName, outputTopicName);
-    }
-}
